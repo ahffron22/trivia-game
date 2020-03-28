d85a0ef28f4b26d7fb40d6e76c3d87dceb10c819
ahffron <ahffron22@gmail.com>
2020-03-27 19:22:35 -0600

    partially functioning buttons

--- actions.js
+++ actions.js
@@ -1,32 +1,60 @@
-var number = 30;
+$(document).ready(function() {
+  // timer
+  var number = 30;
 
-var intervalId;
+  var intervalId;
 
-function run() {
-  clearInterval(intervalId);
-  intervalId = setInterval(decrement, 1000);
-}
-
-function decrement() {
-  number--;
-
-  $(".timer").html("<h2>" + number + "</h2>");
-
-  if (number === 0) {
-    stop();
-
-    alert("Time Up!");
+  function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
   }
-}
 
-function stop() {
-  clearInterval(intervalId);
-}
+  function decrement() {
+    number--;
 
-run();
-//_____________________________________________________________________
+    $(".timer").html("<h2>" + number + "</h2>");
 
-function userGuess() {
-  alert("You lose!");
-}
-$("<td>").on("click", userGuess);
+    if (number === 0) {
+      stop();
+
+      alert("Time Up!");
+    }
+  }
+
+  function stop() {
+    clearInterval(intervalId);
+  }
+
+  run();
+  // answer check
+  var userGuess = "";
+  var correct = "Correct!";
+  var wrong = "Wrong!";
+  var answer = $("right");
+  // $(".choice").click(function() {
+  //   userGuess = $(this);
+  //   if (userGuess === answer) {
+  //     $("#answer").text(correct);
+  //   } else {
+  //     $("#answer").text(wrong);
+  //   }
+  // });
+  $("#a1").click(function() {
+    $("#answer").text(correct);
+  });
+  $("#w1").click(function() {
+    $("#answer").text(wrong);
+  });
+  $("#a2").click(function() {
+    $("#answer2").text(correct);
+  });
+  $("#w2").click(function() {
+    $("#answer2").text(wrong);
+  });
+  $("#a3").click(function() {
+    $("#answer3").text(correct);
+  });
+  $("#w3").click(function() {
+    $("#answer3").text(wrong);
+  });
+});
